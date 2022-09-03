13c34e4de5f988cbedb480a7f9c774103e633d8e
nstppbl <1nstppbl@gmail.com>
2022-09-03 14:14:31 +0500

    initial commit

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -4,8 +4,8 @@ public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
         {
             int n1=0,n2=1,n3,i;
-            System.out.println(n1+" "+n2);//printing 0 and 1
-
+            System.out.println(n1);//printing 0 and 1
+            System.out.println(n2);
             for(i=2;i<lastFibonacci;++i)//loop starts from 2 because 0 and 1 are already printed
             {
                 n3=n1+n2;
